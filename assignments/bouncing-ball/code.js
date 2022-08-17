6c6b0960a11d49a0e388310a7b9c9a7719e3813a
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-17 14:48:50 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -19,6 +19,9 @@ let ballSize = 15;
 // thing we would get from calling now().
 const drawFrame = (time) => {
     drawBall(15 + now(), ballSize)
+    if now() > 20 {
+        now() === 0;
+    };
 };
 
 // Compute the height in pixels at time t after the ball hit the ground
