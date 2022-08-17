8523460253985a5ff67606a9a4117a797ebd96e7
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-17 14:52:05 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -19,7 +19,7 @@ let ballSize = 15;
 // thing we would get from calling now().
 const drawFrame = (time) => {
     drawBall(15 + now(), ballSize)
-    if (now()) = 50 {
+    if (now()) >= 50 {
         now() = 0;
     };
 };
