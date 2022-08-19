383b2cce5720e4062af865d3fed55bd6d9fa7f6d
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-19 15:13:19 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -65,4 +65,4 @@ drawFilledCircle(120, 100, 50, 'blue');
 drawFilledRect(150, 350, 200, 200, 'brown')
 drawLine(200, 300, 300, 400, 'brown', 150);
 drawLine(150, 350, 350, 350, 'black', 1)
-drawFilledRect(250, 400, 50, 200, 'black');
+drawFilledRect(250, 450, 50, 200, 'black');
