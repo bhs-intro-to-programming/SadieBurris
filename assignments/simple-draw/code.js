8d9f8c53b3f4710a8295d4294b1a1e4d8b9bb11c
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-19 15:22:55 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -50,13 +50,15 @@ drawFilledRect(0, 0, 500, 600, 'blue');
 //ground
 drawFilledRect(0, 480, 500, 200, 'green');
 
+
+//sun
+drawFilledCircle(400, 100, 50, 'yellow');
+
 //cloud
 drawFilledCircle(330, 120, 30, 'white');
 drawFilledCircle(360, 130, 20, 'white');
 drawFilledCircle(300, 130, 20, 'white');
 
-//sun
-drawFilledCircle(400, 100, 50, 'yellow');
 
 //moon
 drawFilledCircle(100, 100, 50, 'white');
