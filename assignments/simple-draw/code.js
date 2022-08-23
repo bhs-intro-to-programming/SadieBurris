51ab835cc900cfbc4e194a90873b8e6109959d32
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-23 15:06:04 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -50,7 +50,7 @@ drawFilledRect(0, 0, 500, 600, 'blue');
 drawFilledRect(0, 480, 500, 200, 'green');
 
 //sun
-const sunX = 200
+const sunX = 400
 const sunY = 100
 drawFilledCircle(400, 100, 50, 'yellow');
 
