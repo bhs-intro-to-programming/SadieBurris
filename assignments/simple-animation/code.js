9797097a321cc54c52eb40c940f06b213351910b
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-25 15:18:42 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -45,7 +45,7 @@ let y = 100;
 
 const drawMovingThing = (time) => {
     clear();
-    drawFilledCircle(((time) % (width - 100)) + 50, ((time) % (height - 100)) + 50, 50, 'blue');
+    drawFilledCircle(((time/2) % (width - 100)) + 50, ((time/2) % (height - 100)) + 50, 50, 'blue');
 };
 
 animate(drawMovingThing);
