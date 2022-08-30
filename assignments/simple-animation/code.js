5aef7ade3bb93a342a2b9027d17b154605d8ce3d
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-30 15:05:37 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -4,15 +4,15 @@ let start = now();
 //let x = 100;
 
 const drawMovingThing = () => {
-    let y = ((now() - start)/3);
-    let currentX = x + ((now() - start)/5);
-    clear();
-    drawFilledRect(0, 0, 500, 500, 'blue');
-    drawFilledCircle(currentX, y, 5, 'white');
-    if (currentX >= width || y >= height) {
-        x = Math.random() * width;
-        start = now();
-    };
+  let y = ((now() - start)/3);
+  let currentX = x + ((now() - start)/5);
+  clear();
+  drawFilledRect(0, 0, 500, 500, 'blue');
+  drawFilledCircle(currentX, y, 5, 'white');
+  if (currentX >= width || y >= height) {
+      x = Math.random() * width;
+      start = now();
+  };
 };
 
 animate(drawMovingThing);
