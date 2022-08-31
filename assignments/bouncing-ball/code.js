0c28dd4af59201e75c7bd64cf204b58931a01d5f
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-08-31 15:29:54 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -2,10 +2,11 @@
 let bounce = 0.7;
 let gravity = 0.001;
 let ballSize = 15;
+let start = now()
 
 const drawFrame = (time) => {
    drawBall(time/15, ballSize)
-    if (time/15 <= 100) {
+    if (now() - start/15 <= 100) {
       
     }
 };
