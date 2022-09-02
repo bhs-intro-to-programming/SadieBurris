d0891d72d5b6d0c4864ea73619cfe50cb024da8e
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-02 15:23:52 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -21,7 +21,7 @@ animate(drawMovingThing);
 const drawBouncingThing = (time) => {
   clear();
   let left = time / 10 % (width * 2);
-  let right = (width * 2) - (time / 10) - width % (width * 2)
+  let right = width - time / 10
   if (Math.min(left, right) === left) {
     drawFilledCircle(left, 200, 5, 'blue');
 } else {
