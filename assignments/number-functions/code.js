2a61a8c49054d89df55c734ec61e94d8e5f9b533
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-06 14:55:49 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -5,3 +5,4 @@ const divide = (num1, num2) => num1 / num2;
 const mod = (num1, num2) => num1 % num2;
 const averageOfTwo = (num1, num2) => (num1 + num2) / 2;
 const averageOfThree = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+const distance = (num1, num2) => Math.abs(num1 - num2);
