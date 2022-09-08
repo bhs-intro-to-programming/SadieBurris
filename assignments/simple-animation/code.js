3123e498abdaecfa56662798888b1d6e1a372c8e
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-08 15:04:30 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -36,11 +36,11 @@ drawTriangle(width/2, 0, 0, height, width, height);
 const makeSir = (time) => {
   const x1 = width/2 - (1/2 ** time) * width/2;
   const y1 = 1/2 ** time * width;
-  const x2 =;
-  const y2 =;
-  const x3 =;
-  const y3 =;
-  drawTriangle(x1, y1, )
+  const x2 = x1;
+  const y2 = width/2 + (1/2 ** time) * width/2;
+  const x3 = width/2;
+  const y3 = height * 1/2 ** time;
+  drawTriangle(x1, y1, x2, y2, x3, y3)
 } 
 
 makeSir();
