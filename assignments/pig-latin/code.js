d1e0b4811ada4bc5fbbee938e6cad1657ebd498b
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-09 14:57:51 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -34,7 +34,7 @@
 
 const pigLatin = (s) => s.substring(s.search(/[aeiou]/)) + s.substring(0, s.search(/[aeiou]/)) + 'ay';
 const advancedPigLatin = (s) => {
-  if (s[0] === (a || e || i || o || u)) {
+  if (s[0] === a || s[0] === e || s[0] === i || s[0] === o || s[0] === u) {
   return s + 'way'
   } else {
   return s.substring(s.search(/[aeiou]/)) + s.substring(0, s.search(/[aeiou]/)) + 'ay';
