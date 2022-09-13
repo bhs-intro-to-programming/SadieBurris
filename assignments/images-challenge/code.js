2e94475d995180bd91235a1d00c409ec5025eeae
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-13 14:57:09 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -41,7 +41,7 @@ const drawTarget = (numRing) => {
     } else {
       color = 'blue';
     }
-    drawFilledCircle(width / 2, height / 2, (numRing - i) * (i / numRing), color);
+    drawFilledCircle(width / 2, height / 2, (numRing - i) * ((i + 1) / numRing), color);
   }
 }
 
