d4f83ebd81e6a169b2f3bbb6a817c18296d13745
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-20 14:40:29 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -25,8 +25,8 @@
  */
 
 const drawLineCircle = (r) => {
-  for(let i = 0; r * 2 * i < width; i++) {
-    drawFilledCircle(r * 2 * i + r * 2, 200, r, 'red');
+  for(let i = 0; r * 2 * i - 1 < width; i++) {
+    drawFilledCircle(r * 2 * i, 200, r, 'red');
   }
 }
 
