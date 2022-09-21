b86dcd4e855b63c34465604bfa2344326200a161
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-21 15:16:45 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -80,10 +80,10 @@ const drawLineyCurve = (numLines) => {
 
 const drawRectCircle = (r) => {
   const d = 2 * r
-  const numCircWid = Math.floor(width / d)
-  const numCircHei = Math.floor(height / d)
-  const startWid = (width - numCircleWid * d) / 2
-  const startHei = (height - numCircleHei * d) / 2
+  const numCircWid = Math.floor(width / d);
+  const numCircHei = Math.floor(height / d);
+  const startWid = (width - numCircleWid * d) / 2;
+  const startHei = (height - numCircleHei * d) / 2;
   for (let i = 0; d * i < numCircWid * d; i++) {
     for (let j = 0; d * j < numCircHei * d; j++)
       drawCircle(d * i + startWid, d * j + startHei, r, 'red', 1);
