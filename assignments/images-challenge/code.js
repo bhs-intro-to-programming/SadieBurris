096ffffd1ef968b42e8c714a529f68dc40af9d4a
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-22 14:40:11 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -124,7 +124,7 @@ drawSqrCrc(25);
 
 const drawSqrCrc = (r) => {
   const distCirc = Math.floor(width / (r * 2)) * r * 2;
-  for (let i = 0; distCirc + 1 >= i; i++) {
+  for (let i = 0; distCirc >= i; i++) {
     drawCircle(r * 2 * i + r + (width - distCirc), height - distCirc, r, 'blue');
   }
 }
