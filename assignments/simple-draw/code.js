30aa1e32fff7828a9558d3312c797c694a66608b
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-27 15:27:06 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -56,7 +56,7 @@ drawTriangle(width * 17/32, height * 3/16, width * 19/32, height * 3/16, width *
 */
 
 const autoTri = (x1, y1, wid) => {
-  drawTriangle(x1, y1, x1 + wid, y1, x1 + wid / 2, y1 + wid * 5/16)
+  drawTriangle(x1, y1, x1 + wid, y1, x1 + wid / 2, y1 + wid * 7/16)
 }
 
 drawTriangle(width / 2, 0, 0, height, width, height);
