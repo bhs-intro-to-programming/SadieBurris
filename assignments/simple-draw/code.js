151b925328498e9aba221796fa93c593b3f0cba5
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-28 14:36:16 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -66,7 +66,7 @@ let i = 0;
 const sir = (x, y, wid, depth) => {
   autoTri(x, y, wid)
   if (depth > 0) {
-  sir(x - wid / 4, y + wid * 7/16, wid / 2)
+  sir(x - wid / 4, y + wid * 7/32, wid / 2)
   sir(x + wid / 4, y - wid * 7/32, wid / 2)
   sir(x + wid * 3/4, y + wid * 7/32, wid / 2)
   }
