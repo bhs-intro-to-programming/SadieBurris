4d60b08d861a581a914a9fba19d6c47b59b4828b
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-09-28 14:46:53 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -65,11 +65,11 @@ drawTriangle(width / 2, 0, 0, height, width, height);
 
 const sir = (x, y, wid, depth) => {
   autoTri(x, y, wid)
+  depth = depth - 1;
   if (depth > 0) {
   sir(x - wid / 4, y + wid * 7/32, wid / 2)
   sir(x + wid / 4, y - wid * 7/32, wid / 2)
   sir(x + wid * 3/4, y + wid * 7/32, wid / 2)
-  depth = depth - 1;
   }
 }
 
