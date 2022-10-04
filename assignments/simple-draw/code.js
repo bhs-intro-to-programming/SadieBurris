99fb9953dc288781bb87da1202058735c1152e18
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-04 14:39:07 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -101,8 +101,8 @@ const reCarp = (x, y, widX, widY, depth) => {
   if (depth > 0) {
     for (let exe = 0; exe < 3; exe++) {
       for (let wiy = 0; wiy < 3; wiy++) {
-        let opX = exe === 0 ? reSub(x, widX) : let opX = exe === 1 ? reAdd1(x, widX) : reAdd2(x, widX)
-        let opY = wiy === 0 ? reSub(y, widY) : let opY = wiy === 1 ? reAdd1(y, widY) : reAdd2(y, widY)
+        let opX = exe === 0 ? reSub(x, widX) : exe === 1 ? reAdd1(x, widX) : reAdd2(x, widX)
+        let opY = wiy === 0 ? reSub(y, widY) : wiy === 1 ? reAdd1(y, widY) : reAdd2(y, widY)
         carp(opX, opY, widX / 3, widY / 3, depth - 1)
       }
     }
