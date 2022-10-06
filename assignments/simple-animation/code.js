57d05647cec30e8eb78fda1a73380868405467fb
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-06 15:28:04 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -47,7 +47,11 @@ const gameOfLife = () => {
     for(let j = 0; j < height; j++) {
       let color = Math.random() > 0.9 ? 'black' : 'white'
       drawFilledRect(i, j, 1, 1, color)
-      let assign = color === 'black' ? deadOrNo[i][j] = 1 : deadOrNo[i][j] = 0
+      if(color === 'black') {
+        deadOrNo[i][j] = 1;
+      } else {
+        deadOrNo[i][j] = 0;
+      }
     }
   }
 }
