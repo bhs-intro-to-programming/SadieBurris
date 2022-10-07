1d7f01b9769b2b8546c4f2d767635fcc8ab5a542
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-07 15:29:01 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -44,7 +44,7 @@ const drawPieces = () => {
   const pieceOffset = (width - Math.min(width, height)) / 2 + pieceSize / 2;
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; i++) {
-      drawText(locations[i][j], pieceOffset + i * squareSize, 5 + pieceSize + j * squareSize, 'black', pieceSize);
+      drawText(locations[j - 1][i - 1], pieceOffset + i * squareSize, 5 + pieceSize + j * squareSize, 'black', pieceSize);
     }
   }
 }
