c58d7fd4e66b8bad7c7d64944c9af37a4daaba6b
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-07 14:39:09 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -20,7 +20,7 @@ const drawChess = () => {
   const sqrWidth = checkWidth / 8;
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      let color = (i % 2 === 1 && j % 2 === 0) || (i % 2 === 0 && j % 2 === 1) ? 'black' : 'white';
+      let color = (i % 2 === 1 && j % 2 === 0) || (i % 2 === 0 && j % 2 === 1) ? 'black' : 'gray';
       drawFilledRect(offset + j * sqrWidth, i * sqrWidth, sqrWidth, sqrWidth, color);
     }
   }
