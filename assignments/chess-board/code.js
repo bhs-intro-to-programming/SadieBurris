639c722ca4d897f9005bb06f309d371aec42a9fc
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-07 20:25:51 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -41,8 +41,8 @@ drawChessBoard()
 let squareSize = 0;
 const drawPieces = () => {
   squareSize = Math.min(width, height) / 8;
-  /*
   const pieceSize = squareSize - 10;
+  /*
   const pieceOffset = (width - Math.min(width, height)) / 2 + pieceSize / 2 + 5;
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; i++) {
