29217f0fb766589051860c1208ed70a501b3348a
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-17 15:05:09 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -59,10 +59,11 @@ const gameOfLifeStart = () => {
 }
 const neighbors2 = (i, j) => {
   let neighborsNum = 0
-  let checkPosJ = j + countY === -1 ? height - 1 : j + countY === height ? 0 : j + countY
-  let checkPosI = i + countX === -1 ? width - 1 : i + countX === width ? 0 : i + countX
+  
   for (let countX = -1; countX < 2; countX++) {
     for (let countY = -1; countY < 2; countY++) {
+      let checkPosJ = j + countY === -1 ? height - 1 : j + countY === height ? 0 : j + countY
+  let checkPosI = i + countX === -1 ? width - 1 : i + countX === width ? 0 : i + countX
       if (deadOrNo1[checkPosJ][checkPosI] === 1) {
         neighborsNum++
       }
@@ -72,10 +73,11 @@ const neighbors2 = (i, j) => {
 }
 const neighbors1 = (i, j) => {
   const neighborsNum = 0;
-  let checkPosJ = j + countY === -1 ? height - 1 : j + countY === height ? 0 : j + countY
-  let checkPosI = i + countX === -1 ? width - 1 : i + countX === width ? 0 : i + countX
+  
   for (let countX = -1; countX < 2; countX++) {
     for (let countY = -1; countY < 2; countY++) {
+      let checkPosJ = j + countY === -1 ? height - 1 : j + countY === height ? 0 : j + countY
+  let checkPosI = i + countX === -1 ? width - 1 : i + countX === width ? 0 : i + countX
       if (deadOrNo2[checkPosJ][checkPosI] === 1) {
         neighborsNum++
       }
@@ -150,4 +152,4 @@ const gameOfLife = () => {
   gameOfLife1()
 }
 
-gameOfLife()
+//gameOfLife()
