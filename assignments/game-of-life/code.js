2f423b1c29fac38d3808b0cd7c6705caeb154100
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-19 15:27:37 -0700

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -54,7 +54,7 @@ const newLife1 = (nei, i, j) => {
       return false
     }
   } else if (deadOrNo2[j][i] === 0) {
-    if (nei === 3) {
+    if (nei === 3 || nei === 6) {
       return true
     } else {
       return false
@@ -71,7 +71,7 @@ const newLife2 = (nei, i, j) => {
       return false
     }
   } else if (deadOrNo1[j][i] === 0) {
-    if (nei === 3) {
+    if (nei === 3 || nei === 6) {
       return true
     } else {
       return false
