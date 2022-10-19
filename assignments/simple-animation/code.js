45c4382a0b732d5cd9584af3c3dc729e0ddc511a
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-19 15:26:10 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -95,7 +95,7 @@ const newLife1 = (nei, i, j) => {
       return false
     }
   } else if (deadOrNo2[j][i] === 0) {
-    if (nei === 3 || nei === 6) {
+    if (nei === (3 || 6)) {
       return true
     } else {
       return false
@@ -112,7 +112,7 @@ const newLife2 = (nei, i, j) => {
       return false
     }
   } else if (deadOrNo1[j][i] === 0) {
-    if (nei === 3 || nei === 6) {
+    if (nei === (3 || 6) {
       return true
     } else {
       return false
