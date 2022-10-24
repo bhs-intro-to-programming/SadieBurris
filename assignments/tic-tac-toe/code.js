20d3c601ae44a81c3f49f659348d9b18b75bddc1
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-24 15:32:05 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -30,6 +30,8 @@ registerOnclick((x, y) => {
   for (let i = 0; i < 3; i++) {
     if ((playerLocations[i][0] && playerLocations[i][1] && playerLocations[i][2]) === ('X' || 'O')) {
       console.log('You Won!')
+    } else if ((playerLocations[0][i] && playerLocations[1][i] && playerLocations[2][i]) === ('X' || 'O')) {
+      console.log('YouWon!')
     }
   }
 });
