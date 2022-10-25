509f5d74d2e1809aed4bd258fc9c907727a30ebc
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-25 15:20:41 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -55,9 +55,11 @@ drawPieces()
 const blackPawnRules = (x, y) => {
   if ((locations[y + 1][x - 1] && locations[y + 1][x + 1]) === N) {
     drawFilledRect(offset + x * sqrWidth, (y + 1) * sqrWidth, sqrWidth, sqrWidth, 'red')
-  } else if (locations[y + 1][x - 1] !== N) {
+  } 
+  if (locations[y + 1][x - 1] !== N) {
     drawFilledRect(offset + (x - 1) * sqrWidth, (y + 1) * sqrWidth, sqrWidth, sqrWidth, 'red')
-  } else if (locations[y + 1][x + 1] !== N) {
-    drawFilledRect(offset + (x + 1) * sqrWidth, (y + 1) * sqrWidth, sqrWidth, sqrWidth, 'red')
   }
+  if (locations[y + 1][x + 1] !== N) {
+      drawFilledRect(offset + (x + 1) * sqrWidth, (y + 1) * sqrWidth, sqrWidth, sqrWidth, 'red')
+    }
 }
