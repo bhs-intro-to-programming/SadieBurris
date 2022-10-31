1110228be25ce363a1cc6f4243dd658ece0c2fba
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-10-31 14:57:58 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -136,7 +136,7 @@ const bishopRules = (x, y) => {
       i = 8
     }
   }
-  for (let i = 0; (i + x || i + y) < 8; i++) {
+  for (let i = 0; (x + i || y - i) < 8; i++) {
     if (locations[i - y][i - x] === N) {
       drawFilledRect(offset + (x - i + 1) * sqrWidth + (sqrWidth - 4) / 2, (y + i + 1) * sqrWidth + (sqrWidth - 4) / 2, 4, 4, 'red')
     } else {
