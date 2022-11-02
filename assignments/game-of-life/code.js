7191f6496f0125238cac2897f1e24f4d7e0a04ea
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-02 14:50:53 -0700

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -22,11 +22,11 @@ const gameOfLifeStart = () => {
 
 const neighbors = (i, j) => {
   let neighborsNum = 0
+  const arr = dualSwitcher === 1 ? deadOrNo2[checkPosJ][checkPosI] : deadOrNo1[checkPosJ][checkPosI]
   for (let countX = -1; countX < 2; countX++) {
     for (let countY = -1; countY < 2; countY++) {
       let checkPosJ = j + countY === -1 ? height - 1 : j + countY === height ? 0 : j + countY
       let checkPosI = i + countX === -1 ? width - 1 : i + countX === width ? 0 : i + countX
-      const arr = dualSwitcher === 1 ? deadOrNo2[checkPosJ][checkPosI] : deadOrNo1[checkPosJ][checkPosI]
       if (arr === 1) {
         neighborsNum++
       }
