06c394e11807926570f3d77a6c2187f599b7481e
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-02 15:01:24 -0700

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,20 +1,20 @@
-const deadOrNo1 = []
-const deadOrNo2 = []
+let next = []
+let current = []
 let dualSwitcher = 1;
 
 const gameOfLifeStart = () => {
   for (let j = 0; j < height; j++) {
-    deadOrNo1.push([])
-    deadOrNo2.push([])
+    next.push([])
+    current.push([])
     for (let i = 0; i < width; i++) {
       let color = Math.random() > 0.91 ? 'green' : 'black'
       drawFilledRect(i, j, 1, 1, color)
       if (color === 'green') {
-        deadOrNo2[j].push(1);
-        deadOrNo1[j].push(1);
+        current[j].push(1);
+        next[j].push(1);
       } else {
-        deadOrNo2[j].push(0);
-        deadOrNo1[j].push(0);
+        current[j].push(0);
+        next[j].push(0);
       }
     }
   }
@@ -26,7 +26,7 @@ const neighbors = (i, j) => {
     for (let countY = -1; countY < 2; countY++) {
       let checkPosJ = j + countY === -1 ? height - 1 : j + countY === height ? 0 : j + countY
       let checkPosI = i + countX === -1 ? width - 1 : i + countX === width ? 0 : i + countX
-      const arr = dualSwitcher === 1 ? deadOrNo2[checkPosJ][checkPosI] : deadOrNo1[checkPosJ][checkPosI]
+      const arr = current[checkPosJ][checkPosI];
       if (arr === 1) {
         neighborsNum++
       }
@@ -36,19 +36,18 @@ const neighbors = (i, j) => {
 }
 
 const newLife = (nei, i, j) => {
-  const arr = dualSwitcher === 1 ? deadOrNo2[j][i] : deadOrNo1[j][i]
+  const arr = current[j][i];
   return arr === 1 && (nei === 3 || nei === 4) || arr === 0 && nei === 3
 }
 
 const gameOfLifeFrame = () => {
   for (let j = 0; j < height; j++) {
     for (let i = 0; i < width; i++) {
-      const arr = dualSwitcher === 1 ? deadOrNo1 : deadOrNo2
-      if (newLife(neighbors(i, j), i, j) === true) {
-        arr[j][i] = 1
+      if (newLife(neighbors(i, j), i, j)) {
+        next[j][i] = 1
         drawFilledRect(i, j, 1, 1, 'green')
-      } else if (newLife(neighbors(i, j), i, j) === false) {
-        arr[j][i] = 0
+      } else {
+        next[j][i] = 0
         drawFilledRect(i, j, 1, 1, 'black')
       }
     }
@@ -57,7 +56,7 @@ const gameOfLifeFrame = () => {
 
 const gameOfLife = () => {
   gameOfLifeFrame()
-  dualSwitcher = dualSwitcher === 1 ? 2 : 1
+  [current, next] = [next, current]
 }
 
 gameOfLifeStart()
