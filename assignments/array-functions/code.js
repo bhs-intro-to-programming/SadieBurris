10a2e0e8be57b22d29dad75156d5c7f9a320eea8
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-03 14:57:35 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -4,18 +4,10 @@ const sum = (ar) => ar.reduce((pre, cur) => pre + cur, 0)
 
 const evens = (ar) => ar.filter(num => num % 2 === 0)
 
-const anyOverOneHundred = (ar) => ar.filter(num => num > 100) > 0 ? true : false
-/*{
-  for (let i = 0; i < ar.length; i++) {
-    if (ar[i] > 100) {
-      return true
-    }
-  }
-  return false
-}
-*/
+const anyOverOneHundred = (ar) => ar.filter(num => num > 100) > 0
 
-const pyramid = (num) => {
+const pyramid = (num) => 
+/*{
   const ar = [];
   for (let i = 0; i <= num; i++) {
     for (let j = 0; j < i; j++) {
@@ -24,3 +16,4 @@ const pyramid = (num) => {
   }
   return ar
 }
+*/
