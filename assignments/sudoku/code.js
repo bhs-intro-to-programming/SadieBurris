e97046859d2fd1a84f9fee69fed6e7b49483b816
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-04 14:45:09 -0700

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -2,7 +2,8 @@ const edgeSize = (width - height) / 2
 
 const drawBoard = () => {
   for (let i = 0; i < 10; i++) {
-    drawLine(edgeSize, height / 9 * i, width - edgeSize, height / 9 * i, 1)
+    const thick = (i - 1) % 3 === 0 ? 4 : 1
+    drawLine(edgeSize, height / 9 * i, width - edgeSize, height / 9 * i, thick)
   }
 }
 drawBoard()
