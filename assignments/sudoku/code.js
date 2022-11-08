2294f91aaa297e591180e7e7f403fdf79450122b
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-08 15:03:27 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -11,13 +11,13 @@ const drawBoard1st = () => {
 }
 
 registerOnclick((x, y) => {
-  if (x > edgeSize && x < width - edgeSize) {
+  if (x < edgeSize && y < 20) {
+    let selected = Math.floor(x / (edgeSize / 10))
+  } else if (x > edgeSize && x < width - edgeSize) {
     const exe = Math.floor((x - edgeSize) / (height / 9))
     const wiy = Math.floor(y / (height / 9))
     b[wiy][exe] = 'X'
     drawText('X', edgeSize + exe * (height / 9) + height / 64, wiy * (height / 9) + height * 7 / 64, 'black', height / 9)
-  } else if (x < edgeSize && y < 20) {
-    let selected = Math.floor(x / (edgeSize / 10))
   }
 })
 
