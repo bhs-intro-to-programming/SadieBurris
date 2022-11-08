e3e7c3902243a1011a741cdf6ff9067ddada790e
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-08 14:43:05 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -5,13 +5,12 @@ const distance = (point1, point2) => Math.sqrt(Math.abs(point1.x - point2.x) ** 
 const midpoint = (point1, point2) => ({ x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2 })
 const sumSalaries = (ar) => ar.reduce((pre, cur) => cur.salary + pre, 0)
 const newHighScore = (high, ar) => {
-  let newScore = high
   for (let i = 0; i < ar.length; i++) {
     if (ar[i].score > newScore) {
-      newScore = ar[i].score
+      high = ar[i].score
     }
   }
-  return newScore
+  return high
   //return to later plz its too long
 }
 const summarizeBooks = (ar) => {
