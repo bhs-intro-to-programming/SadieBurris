8f46704764605d26ae571a08e69b38d4cbbb7500
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-08 14:41:42 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,10 +2,7 @@ const getX = (ob) => ob.x
 const point = (num1, num2) => ({ x: num1, y: num2 })
 const emptyObject = () => ({})
 const distance = (point1, point2) => Math.sqrt(Math.abs(point1.x - point2.x) ** 2 + Math.abs(point1.y - point2.y) ** 2)
-const midpoint = (point1, point2) => {
-  const result = { x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2 }
-  return result
-}
+const midpoint = (point1, point2) => ({ x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2 })
 const sumSalaries = (ar) => ar.reduce((pre, cur) => cur.salary + pre, 0)
 const newHighScore = (high, ar) => {
   let newScore = high
