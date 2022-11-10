cf770bfba6256d28799fb45a455117e5d37ceb03
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-10 15:29:04 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -7,8 +7,8 @@ const drawBoard1st = () => {
     const thick = i % 3 === 0 ? 3 : 1
     drawLine(edgeSize, height / 9 * i, width - edgeSize, height / 9 * i, 'black', thick)
     drawLine(edgeSize + height / 9 * i, 0, edgeSize + height / 9 * i, height, 'black', thick)
-    if(i !== 9) {
-    drawText(i + 1, edgeSize / 10 * i, 20, 'black', 20)
+    if (i !== 9) {
+      drawText(i + 1, edgeSize / 10 * i, 20, 'black', 20)
     }
   }
 }
@@ -34,13 +34,25 @@ const lineNine = () => {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (b[j][i].length === 9 && b[j][i][0] === '') {
-        for (let p = 1; p < 10; p++){
+        for (let p = 1; p < 10; p++) {
           if (b[j][i].indexOf(p) === -1) {
             drawText(p, edgeSize + i * (height / 9) + height / 64, j * (height / 9) + height * 6 / 64, 'gray', height / 9)
             b[j][i][0] = p
+            for (let i = 0; i < 9; i++) {
+              b[row][i].push(selected)
+              b[i][col].push(selected)
+            }
           }
         }
       }
+    }
+  }
+}
+
+squareNine = () => {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      //you get the point future me 0_0
     }
   }
 }
