699a243d49715cc29e0a889d459d4d796db1d5cf
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-15 14:35:04 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -1,8 +1,7 @@
 // For a change of pace, I'm providing you with all the function skeletons. This
 // should save you some time.
 
-const area = (rect) => {
-};
+const area = (rect) => rect.width * rect.height
 
 const higherPaid = (e1, e2) => {
 };
