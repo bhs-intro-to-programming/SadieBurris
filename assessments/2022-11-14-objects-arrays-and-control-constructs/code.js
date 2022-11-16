2d7f0bab43c7b82725a64760afc39bc1f3c2a9a2
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-16 15:13:01 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -36,3 +36,4 @@ const sums = (n) => {
 }
 
 const rule110 = (cells) => cells.map(num => ((cells[num - 1] === 1) && (cells[num] === cells[num + 1])) || ((cells[num - 1] && cells[num] && cells[num + 1]) === 0) ? 0 : 1)
+//Idk im tired and bored and want to do something else
