87900572bf0fb8aa034b5368d1382171658260c4
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-11-16 15:08:56 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -12,11 +12,9 @@ const totalWithTip = (sT, percentage) => ({ subtotal: sT.subtotal, tip: percenta
 const isWinner = (player) => player.score > 100
 
 const updateWins = (players) => {
-  return players.map(i => isWinner(players[i]) ? players[i].wins++ : players[i])
-  /*for (let i = 0; i < players.length; i++) {
+  for (let i = 0; i < players.length; i++) {
     if (isWinner(players[i])) players[i].wins++;
   }
-  */
 };
 
 const bigWinners = (players) => players.filter((o) => o.wins > 10)
