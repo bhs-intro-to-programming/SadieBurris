5b41415642306eb1d2a5fbd746ab303e084c2d6a
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-02 14:51:55 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -100,6 +100,7 @@ const solveBranch = (row, col, curPath) => {
   } else {
     for (let i = 0; i < 4; i++) {
       if (b[row][col][i + 1] === true) {
+        return curPath
         //solveBranch(directionsRows[i], directionsCols[i], curPath)
       }
     }
