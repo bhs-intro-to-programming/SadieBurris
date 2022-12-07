d592813593d9fe5e67c3957fbe8e4faa309eed8e
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-07 14:56:54 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -17,8 +17,7 @@ const solveBranch = (row, col, curPath) => {
   if (row === 8 && col === 8) {
     curPath.push(8, 9)
     for (let i = 0; i < curPath.length / 2; i++) {
-      drawLine(0,0,100,100,'red', 4)
-      //drawLine(curPath[i * 2 * (height / 9)] + edgeSize + height / 18, curPath[(i * 2 + 1) * (height / 9)] + height / 18, curPath[(i * 2 + 3) * (height / 9)] + edgeSize + height / 18, curPath[(i * 2 + 4) * (height / 9)] + height / 18, 'red', 4)
+      drawLine(curPath[i * 2 * (height / 9)] + edgeSize + height / 18, curPath[(i * 2 + 1) * (height / 9)] + height / 18, curPath[(i * 2 + 3) * (height / 9)] + edgeSize + height / 18, curPath[(i * 2 + 4) * (height / 9)] + height / 18, 'red', 4)
     }
   } else {
     for (let i = 0; i < 4; i++) {
