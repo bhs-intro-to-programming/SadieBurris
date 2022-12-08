26fef076303822544e9a21c8914bafd42debbe6f
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-08 14:56:35 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -1,5 +1,4 @@
 const buffer = (width - height) / 2
-const num = 2
 const b = Array(4).fill().map(() => Array(4).fill().map(() => Array(2).fill('')))
 
 const drawStart = () => {
@@ -11,6 +10,7 @@ const drawStart = () => {
 }
 
 registerOnKeyDown((key) => {
+  const num = Math.random() < 0.8 ? 2 : 4
   console.log(num, key)
 })
 
