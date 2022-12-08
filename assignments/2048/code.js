38d0dcf4b1eb8dbdb9c9db4a8be4755149008f51
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-08 15:08:01 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -1,4 +1,5 @@
 const buffer = (width - height) / 2
+const arrowToNum = [ArrowRight, ArrowLeft, ArrowUp, ArrowDown]
 const b = Array(4).fill().map(() => Array(4).fill().map(() => Array(2).fill('')))
 
 const drawStart = () => {
@@ -12,7 +13,7 @@ const drawStart = () => {
 registerOnKeyDown((key) => {
   const num = Math.random() < 0.7 ? 2 : 4
   const placement = [[[Math.floor(Math.random() * 4)], [0]], [[Math.floor(Math.random() * 4)], [3]], [[3], [Math.floor(Math.random() * 4)]], [[0], [Math.floor(Math.random() * 4)]]]
-  console.log(key, num, placement[0])
+  console.log(key, num, placement[arrowToNum.locate(key)])
 })
 
 drawStart()
