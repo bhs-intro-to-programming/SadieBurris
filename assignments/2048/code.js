61b7d8ad881616ca29426233a4757105172979e3
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-08 14:46:57 -0800

    Updating assignments/2048/code.js

---
+++ assignments/2048/code.js
@@ -0,0 +1,11 @@
+const buffer = (width - height) / 2
+const b = Array(4).fill().map(() => Array(4).fill().map(() => Array(2).fill('')))
+
+const drawStart = () => {
+  for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 4; col++) {
+      drawRect(buffer + col * (height / 4), row * (height / 4), height / 4, height / 4, 'black')
+    }
+  }
+}
+drawStart()
