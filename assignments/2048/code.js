2b43e2d17c0dc87970419ab7dc1d6feb64fc48bd
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-09 15:00:50 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -13,7 +13,7 @@ registerOnKeyDown((key) => {
   const num = Math.random() < 0.7 ? 2 : 4
   const keyToNum = key === 'ArrowRight' ? 0 : key === 'ArrowLeft' ? 1 : key === 'ArrowUp' ? 2 : key === 'ArrowDown' ? 3 : 'error'
   const placement = [[[Math.floor(Math.random() * 4)], [0]], [[Math.floor(Math.random() * 4)], [3]], [[3], [Math.floor(Math.random() * 4)]], [[0], [Math.floor(Math.random() * 4)]]]
-  console.log(key, num, placement(keyToNum))
+  console.log(key, num, placement[keyToNum])
 })
 
 drawStart()
