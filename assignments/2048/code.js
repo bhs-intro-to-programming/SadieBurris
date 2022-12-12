4f3c72ef86600c2b041d904f380a269544564406
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-12 15:06:34 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -13,16 +13,16 @@ const locPlaced = (num, row, col) => {
   //ill make it short later
   let result = [num, 0, 0, 0, 0]
   for (let i = 1; i < 4; i++) {
-    if (b[row][col + i] !== undefined) {
+    if (b[row][col + i][0] !== undefined) {
       result[1]++
     }
-    if (b[row][col - i] !== undefined) {
+    if (b[row][col - i][0] !== undefined) {
       result[2]++
     }
-    if (b[row - 1][col] !== undefined) {
+    if (b[row - 1][col][0] !== undefined) {
       result[3]++
     }
-    if (b[row + 1][col] !== undefined) {
+    if (b[row + 1][col][0] !== undefined) {
       result[4]++
     }
   }
