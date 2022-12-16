f838ac1bfcafcbb0eb7ecf30e3ac8d12ffb29545
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-16 11:42:30 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -52,7 +52,7 @@ const newSquare = (row, col) => [row, col + 1, row, col - 1, row - 1, col, row +
 let firstTime = true
 
 const appleEaten = () => {
-
+  b[Math.random * numOfSquares][Math.random * numOfSquares][1] === 1
 }
 
 const drawBoard = () => {
