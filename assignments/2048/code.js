dea83ec706e50fe10d2075e17b4347fe3cacb6a2
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2022-12-16 11:13:58 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -65,5 +65,5 @@ drawBoard(10)
 
 registerOnKeyDown((key) => {
   const keyToNum = key === 'ArrowRight' ? 0 : key === 'ArrowLeft' ? 1 : key === 'ArrowUp' ? 2 : key === 'ArrowDown' ? 3 : 'error'
-  b[newSquare(keyToNum)][keyToNum + 1] = snakeLength
+  b[newSquare(keyToNum)][newSquare(keyToNum + 1)] = snakeLength
 })
